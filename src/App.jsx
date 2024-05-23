6f711489872668d96ff070bcd3a31ab40ccb551b
Manish <mb2060127@gmail.com>
2024-05-23 10:41:36 +0545

    chore: Remove unused variable and whitespace in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
- const name = "Manish"
+
 
   return (
     <>
@@ -19,7 +19,7 @@ function App() {
       </div>
       <h1>Sandip + React</h1>
       <div className="card">
-        <h2>Hello {name}</h2>
+   
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
